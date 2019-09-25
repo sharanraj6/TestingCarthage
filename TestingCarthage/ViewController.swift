0b29bee297d76aa303cb050421d4745c7d929518
Murugan <murugan@Murugans-iMac.local>
2019-09-25 12:52:18 +0530

    initial commit

---
+++ TestingCarthage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestingCarthage
+//
+//  Created by Murugan  on 17/09/19.
+//  Copyright © 2019 Murugan . All rights reserved.
+//
+
+import UIKit
+import SkyFloatingLabelTextField
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+                let textField = SkyFloatingLabelTextField(frame: CGRect(x: 50, y: 200, width: 200, height: 50))
+                textField.placeholder = "Name"
+                textField.title = "Your full name"
+                self.view.addSubview(textField)
+    }
+
+
+}
+
---
+++ TestingCarthageTests/TestingCarthageTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestingCarthageTests.swift
+//  TestingCarthageTests
+//
+//  Created by Murugan  on 17/09/19.
+//  Copyright © 2019 Murugan . All rights reserved.
+//
+
+import XCTest
+@testable import TestingCarthage
+
+class TestingCarthageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
